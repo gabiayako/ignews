71798cf7a598341de0016b195f64dd8a4aec5274
gabiayako <gabiayako@gmail.com>
2021-11-21 18:10:14 -0300

    Saving email on faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,24 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('users_by_email'), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email: user.email } }),
+            q.Get(q.Match(q.Index('users_by_email'), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (e) {
+        return false;
+      }
+    },
+  },
 });
