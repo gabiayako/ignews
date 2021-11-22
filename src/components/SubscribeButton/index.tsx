4de7092e47897c844c198620e4e2896281789ac4
gabiayako <gabiayako@gmail.com>
2021-11-22 08:02:56 -0300

    Implementing subscribe feature - part I

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,38 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => (
-  <button type="button" className={styles.container}>
-    Subscribe now
-  </button>
-);
+export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout(sessionId);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className={styles.container}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
+};
