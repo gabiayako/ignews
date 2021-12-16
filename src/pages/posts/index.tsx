c7ae98ff53ac5470f4dad0090e12eb1244e3e9b5
gabiayako <gabiayako@gmail.com>
2021-12-16 20:08:32 -0300

    Adding post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import Link from "next/link";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
 import { RichText } from "prismic-dom";
@@ -25,13 +26,17 @@ export default function Posts({ posts }: PostsProps) {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updated}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
-          ))}
+          {posts.map((post) => {
+            return (
+              <Link key={post.slug} href={`/posts/${post.slug}`}>
+                <a>
+                  <time>{post.updated}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
+            );
+          })}
         </div>
       </main>
     </>
